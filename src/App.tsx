948dc27bd7f6fbbfaf00e6f956c39c18c9545425
JagTheFriend <jagthefriend12@gmail.com>
2021-11-23 12:50:57 +0200

    The calculator is now working

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,45 @@ import { ACTIONS } from "./constants.d";
 import Button from "./Components/Buttons";
 import OperationButton from "./Components/OperationButton";
 
+function evaluate({
+    currentOperand,
+    previousOperand,
+    operation,
+}: {
+    currentOperand: string;
+    previousOperand: string;
+    operation: string;
+}): string {
+    const prev: number = parseFloat(previousOperand);
+    const current: number = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return "";
+    let computation = 0;
+    switch (operation) {
+        case "+":
+            computation = prev + current;
+            break;
+        case "-":
+            computation = prev - current;
+            break;
+        case "*":
+            computation = prev * current;
+            break;
+        case "÷":
+            computation = prev / current;
+            break;
+    }
+    return computation.toString();
+}
+
 function reducer(
     state: any,
     { type, payload }: { type: ACTIONS; payload: Record<string, string> }
 ) {
     switch (type) {
         case ACTIONS.ADD_DIGIT: {
+            // overwrite the previous result
+            if (state.overwrite)
+                return { ...state, currentOperand: payload.digit, overwrite: false };
             // there can only be 1 zero at the start
             if (payload.digit === "0" && state.currentOperand === "0") return state;
             // the user can have only 1 period
@@ -18,6 +51,52 @@ function reducer(
             return {
                 ...state,
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+            };
+        }
+        case ACTIONS.CHOOSE_OPERATION: {
+            if (state.currentOperand == null && state.previousOperand == null) {
+                return state;
+            }
+
+            if (state.currentOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                };
+            }
+
+            if (state.previousOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                    previousOperand: state.currentOperand,
+                    currentOperand: null,
+                };
+            }
+
+            return {
+                ...state,
+                previousOperand: evaluate(state),
+                operation: payload.operation,
+                currentOperand: null,
+            };
+        }
+        case ACTIONS.CLEAR: {
+            return {};
+        }
+        case ACTIONS.EVALUATE: {
+            if (
+                state.operation == null ||
+                state.currentOperand == null ||
+                state.previousOperand == null
+            )
+                return state;
+            return {
+                ...state,
+                currentOperand: evaluate(state),
+                overwrite: true,
+                previousOperand: null,
+                operation: null,
             };
         }
     }
@@ -57,7 +136,12 @@ function App() {
 
             <Button digit="." dispatch={dispatch} />
             <Button digit="0" dispatch={dispatch} />
-            <button className="span-two">=</button>
+            <button
+                className="span-two"
+                onClick={() => dispatch({ type: ACTIONS.EVALUATE, payload: {} })}
+            >
+                =
+            </button>
         </div>
     );
 }
